cb49b96c29402ae46d6163a82b55c6d374d66414
RetaxMaster <retaxmaster@hotmail.com>
2020-11-15 15:49:26 -0600

    Primera version del paquete de NPM

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const messages = [
+    "RetaxMaster",
+    "Caroline",
+    "Paulina",
+    "Ana",
+    "Nicolay",
+    "Yesica",
+    "Diego", 
+    "Laura"
+];
+
+const randomMsg = () => {
+
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+
+};
+
+module.exports = { randomMsg };
